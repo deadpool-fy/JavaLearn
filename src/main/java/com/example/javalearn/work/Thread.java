e222e1a775e92406d6f98799596392e17e187e1b
fangyuan <deadpool4fy@gmail.com>
2024-11-16 21:07:03 +0800

    feat： 创建线程池

--- src/main/java/com/example/javalearn/work/Thread.java
+++ src/main/java/com/example/javalearn/work/Thread.java
@@ -1,4 +1,18 @@
 package com.example.javalearn.work;
 
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class Thread {
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3, 5,
+                3, TimeUnit.SECONDS, new ArrayBlockingQueue<>(5),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardOldestPolicy());
+
+    }
 }
